cc6a59ab51846fe2d019b89de06a7904d320b8f9
winfish <zhiyong@huskers.unl.edu>
2020-05-16 09:40:39 -0500

    update main.c

--- car/car/main.c
+++ car/car/main.c
@@ -1,62 +1,40 @@
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include <stdbool.h>
-#include <util/delay.h>
+#define F_CPU 16000000UL     //8MHz CPU frequency
+#include <util/delay.h>     //To use _delay_ms()
 
-int main (void) {
+void pwm_init()     //to initialize PWM
+{
+    /*
+	TCCR0A
+	-------------------------------------------------------------
+	| COM0A1 | COM0A0 | COM0B1 | COM0B0 | - | - | WGM01 | WGM00 |
+	-------------------------------------------------------------
+	FastPWM mode: COM0x1:0 =	0 normal,OC0x disconnected; 
+								1 Toggle OC0x on compare match; 
+								2 clear OC0x on compare match, set at bottom
+								3 set OC0x on compare match, clear at bottom
+	
+	*/
+	TCCR0A = 0b10100011;
+	  
+    TCCR1B = 0b00000001;     //register TCCR1B
+    DDRD = 0xff;     //set portd as output 
+}
 
-	/**
-	 * We will be using OCR1A as our PWM output which is the
-	 * same pin as PB1.
-	 */
-	DDRB |= _BV(PB1);
+int main(void)
+{   
+    pwm_init();     //to initialize PWM
+    //D0 and D1 for right motor, D2 and D3 for left motor
+    while(1)
+    {
+        PORTD = 0b00000101;
+        OCR1A = 1023;     //maximum
+        OCR1B = 500;
+        _delay_ms(2000);
 
-	/**
-	 * There are quite a number of PWM modes available but for the
-	 * sake of simplicity we'll just use the 8-bit Fast PWM mode.
-	 * This is done by setting the WGM10 and WGM12 bits.  We 
-	 * Setting COM1A1 tells the microcontroller to set the 
-	 * output of the OCR1A pin low when the timer's counter reaches
-	 * a compare value (which will be explained below).  CS10 being
-	 * set simply turns the timer on without a prescaler (so at full
-	 * speed).  The timer is used to determine when the PWM pin should be
-	 * on and when it should be off.
-	 */
-	TCCR1A |= _BV(COM1A1) | _BV(WGM10);
-	TCCR1B |= _BV(CS10) | _BV(WGM12);
-
-	/**
-	 *  This loop is used to change the value in the OCR1A register.
-	 *  What that means is we're telling the timer waveform generator
-	 *  the point when it should change the state of the PWM pin.
-	 *  The way we configured it (with _BV(COM1A1) above) tells the
-	 *  generator to have the pin be on when the timer is at zero and then
-	 *  to turn it off once it reaches the value in the OCR1A register.
-	 *
-	 *  Given that we are using an 8-bit mode the timer will reset to zero
-	 *  after it reaches 0xff, so we have 255 ticks of the timer until it
-	 *  resets.  The value stored in OCR1A is the point within those 255
-	 *  ticks of the timer when the output pin should be turned off
-	 *  (remember, it starts on).
-	 *
-	 *  Effectively this means that the ratio of pwm / 255 is the percentage
-	 *  of time that the pin will be high.  Given this it isn't too hard
-	 *  to see what when the pwm value is at 0x00 the LED will be off
-	 *  and when it is 0xff the LED will be at its brightest.
-	 */
-	uint8_t pwm = 0x00;
-	bool up = true;
-	for(;;) {
-
-		OCR1A = pwm;
-
-		pwm += up ? 1 : -1;
-		if (pwm == 0xff)
-			up = false;
-		else if (pwm == 0x00)
-			up = true;
-
-		_delay_ms(10);
-	}
-
+        PORTD = 0b00000110;
+        OCR1A = 500;
+        OCR1B = 1023;     //maximum
+        _delay_ms(2000);
+    }
 }
